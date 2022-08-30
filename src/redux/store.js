a5be6b8e3e259b4b9ec1f039b7ecba61b08ff3a7
Vasilescu Andrei <andrei.vasilescu1995@gmail.com>
2022-08-30 17:12:55 +0300

    fixed non serializable warning

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,12 +3,18 @@ import { userSlice } from "./slices/user";
 import { employeesSlice } from "./slices/employees";
 import { snackbarSlice } from "./slices/snackbar";
 import { modalsSlice } from "./slices/modals";
+import { calendarSlice } from "./slices/calendar";
 
 export const store = configureStore({
     reducer: {
         user: userSlice.reducer,
         employees: employeesSlice.reducer,
         snackbar: snackbarSlice.reducer,
-        modals: modalsSlice.reducer
+        modals: modalsSlice.reducer,
+        calendar: calendarSlice.reducer
     },
+    middleware: getDefaultMiddleware =>
+        getDefaultMiddleware({
+            serializableCheck: false,
+        }),
 });
